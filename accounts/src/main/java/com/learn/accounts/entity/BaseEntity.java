7aa8c1a620c703a75b00deb291e6dccb4684aee8
knpragash <knpragash@gmail.com>
2023-09-21 17:24:43 +0800

    Added Customer & Accounts Repositories

--- accounts/src/main/java/com/learn/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/learn/accounts/entity/BaseEntity.java
@@ -2,14 +2,24 @@ package com.learn.accounts.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 
+@MappedSuperclass
 @Data
 public class BaseEntity {
     
+    @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @Column(updatable = false)
     private String createdBy;
+
+    @Column(insertable = false)
     private LocalDateTime updatedAt;
+
+    @Column(insertable = false)
     private String updatedBy;
 
 }
---
+++ accounts/src/main/java/com/learn/accounts/entity/Customer.java
@@ -0,0 +1,31 @@
+package com.learn.accounts.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+// import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+    @GenericGenerator(name="native", strategy="native")
+    private Long customerId;
+
+    private String name;
+
+    private String email;
+
+    private String MobuileNumber;
+    
+}
