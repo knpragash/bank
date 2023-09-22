7f98a44826dc71daaed3d5c2115119ea6dc8b593
knpragash <knpragash@gmail.com>
2023-09-22 18:51:25 +0800

    Completed Create API

--- accounts/src/main/java/com/learn/accounts/entity/Customer.java
+++ accounts/src/main/java/com/learn/accounts/entity/Customer.java
@@ -2,6 +2,7 @@ package com.learn.accounts.entity;
 
 import org.hibernate.annotations.GenericGenerator;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,6 +27,7 @@ public class Customer extends BaseEntity {
 
     private String email;
 
-    private String MobuileNumber;
+    @Column(name="mobile_number")
+    private String mobileNumber;
     
 }
---
+++ accounts/src/main/java/com/learn/accounts/service/IAccountsService.java
@@ -0,0 +1,8 @@
+package com.learn.accounts.service;
+
+import com.learn.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+    
+    public void createAccount(CustomerDto customerDto);
+}
